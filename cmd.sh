f99d4a3c39410cc53a40d0c2b5d0f1d7f5e64068
djluo <dj.luo@baoyugame.com>
2016-02-20 15:35:34 +0800

    使用环境变量中的设置

--- cmd.sh
+++ cmd.sh
@@ -18,7 +18,22 @@ LOG_DIR="${current_dir}/log/"
 export CATALINA_HOME CONFIG CATALINA_OUT LOGGING_CONFIG LOG_DIR CATALINA_TMPDIR
 
 # java参数等配置
-[ -f  "${JDK_OPT}" ] && source "${JDK_OPT}"
+if [ -f  "${JDK_OPT}" ];then
+  source "${JDK_OPT}"
+elif [ -n "$JAVA_OPTS" ] ;then
+  # 使用环境变量中的设置
+  :
+else
+  JAVA_OPTS="-Xms1G -Xmx1G"
+fi
+
+DEF_OPTS="-Dnet.sf.ehcache.skipUpdateCheck=true    \
+          -Dcom.sun.management.jmxremote           \
+          -Dcom.sun.management.jmxremote.ssl=false \
+          -Dcom.sun.management.jmxremote.local.only=false \
+          -Dcom.sun.management.jmxremote.authenticate=false"
+
+export JAVA_OPTS="$JAVA_OPTS $DEF_OPTS"
 
 exec $CATALINA_HOME/bin/catalina.sh run -config $CONFIG \
   1> >(exec /usr/bin/cronolog ${current_dir}/logs/stdout.txt-%Y%m%d >/dev/null 2>&1) \
